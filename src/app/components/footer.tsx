23ac595149ccdf694dcef4a5dc8e35ec9ab83efc
Skym <yannis.perrier@icloud.com>
2025-06-01 00:55:24 +0200

    Update de textes, design, et de responsive

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -4,8 +4,6 @@ const footer = () => {
             <div className="footerChild">
                 <p>skymdev.fr par Skym, fait à Privas</p>
                 <a href="https://www.instagram.com/yns_pri/" target="_blank">Instagram</a>
-                <a href="https://twitter.com/skymdev" target="_blank">Twitter</a>
-                <a href="https://www.threads.net/@yns_pri" target="_blank">Threads</a>
                 <a href="https://discord.gg/3wBPrAzUHH" target="_blank">Discord</a>
                 <a href="mailto:yannis.perrier@icloud.com" target="_blank">Adresse e-mail</a>
             </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,14 +30,16 @@ export default async function Home() {
                 <div className="about">
                     <img src='/skym.png' alt="SkymDev" />
                     <h1>A propos de moi</h1>
-                    <h4>Bonjour ! Moi c'est Skym. Je m'appelle Yannis, je suis développeur.<br/>
-                        Depuis tout petit, j'ai toujours aimé l'informatique et par dessus tout l'espace. J'ai une passion pour tout ce qui vole.<br/>
+                    <h4>Bonjour ! Moi c'est Skym. Je m'appelle Yannis, je suis développeur.
+                        <br/>Depuis tout petit, j'ai toujours aimé l'informatique et par-dessus tout l'espace. J'ai une passion pour tout ce qui vole.<br/>
                     Tout naturellement, j'ai un esprit scientifique et j'aime énormément ce qui touche à la science
                     de manière générale.<br/>
-                    J'ai 17 ans et j'ai mené, depuis quelques années, plusieurs projets.<br/>
-                        Qu'ils soient audiovisuels, informatiques, graphiques, ils sont tous répertoriés plus bas.
-                        Certains sont terminés, d'autres sont en cours de développement ou actifs.<br/>
-                        Ils sont parfois pour des clients, et parfois pour moi-même.<br/>
+                    <br/>J'ai 17 ans et j'ai mené, depuis quelques années, plusieurs projets.<br/>
+                        Qu'ils soient audiovisuels, informatiques, graphiques, ils sont tous répertoriés plus bas.<br/>
+
+                        <br/>Certains sont terminés, d'autres sont en cours de développement ou actifs.<br/>
+                        Ils sont parfois réalisés pour des clients, et parfois pour moi-même. Chacun exploite des compétences différentes,
+                        que j'ai acquises au fil des années de manière entièrement autodidacte.<br/>
                     </h4>
                 </div>
             </section>
@@ -47,7 +49,15 @@ export default async function Home() {
             <section id="dev">
                 <div className="dev">
                     <h1>Freelance</h1>
-                    <h4>En tant que développeur web, je propose mes services sur Fiverr [...].</h4>
+                    <h4>Je travaille avec WebStorm, GitHub, NextJS, React et des headless
+                        CMS (Content Management System) pour concevoir les sites internet,
+                        qu'ils soient de simples sites vitrines ou des boutiques en ligne.
+                        J'écris le code à la main et par conséquent n'utilise aucun éditeur
+                        tel que WordPress, Divi, Webflow... Ainsi, mes clients ont un accès total,
+                        du code source à la gestion du contenu
+                        J'utilise Adobe Photoshop pour les logos et visuels, et
+                    Filmora pour les travaux audiovisuels.
+                    </h4>
                 </div>
             </section>
         </>
